25503e69b1df8da23f85080c5daaaa4f8aa3f43c
Lina Chalakova <linach@abv.bg>
2013-05-16 23:59:25 +0300

    added TestBalloonsPop5Game

---
+++ TestBalloonPop5Game/TestCommandParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBalloonPop5Game
+{
+    [TestClass]
+    public class TestCommandParser
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
